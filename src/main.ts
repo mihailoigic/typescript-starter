5b00eecda1735b5daa1dc5bef3cdd777a2e021de
mihailoigic <mihailoigic@.com>
2023-07-29 22:55:47 +0200

    init commit with changing port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 4000;
+  console.log('Running on port: ' + port);
+  await app.listen(port);
 }
 bootstrap();
